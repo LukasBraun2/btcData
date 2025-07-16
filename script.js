fc7d2a87bbf66ab8299f7a8705315e318279a1e2
Lukas Braun <lukasbruhan@gmail.com>
2025-07-16 10:49:41 -0700

    not working

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ const ws = new WebSocket("wss://stream.binance.com:9443/ws/btcusdt@depth");
 ws.onmessage = (event) => {
   const snapshot = JSON.parse(event.data);
   fs.appendFileSync("btcLog.json", JSON.stringify(snapshot) + "\n");
-<<<<<<< HEAD
   console.log("Snapshot saved."); // For debug
-});
 
 ws.on("error", (err) => {
   console.error("WebSocket error:", err);
@@ -15,6 +13,4 @@ ws.on("error", (err) => {
 ws.on("close", () => {
   console.log("WebSocket closed.");
 });
-=======
 };
->>>>>>> parent of 4cf6dbb (getting data now)
